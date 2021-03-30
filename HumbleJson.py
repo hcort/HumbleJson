e7be5cbbca0b2947fc2ecc05699aeed0c880d9bd
Hector <h.cortiguera@gmail.com>
2021-03-30 17:45:14 +0200

    Refactor long function into smaller steps

--- HumbleJson.py
+++ HumbleJson.py
@@ -13,6 +13,16 @@ import requests
 from bs4 import BeautifulSoup
 
 
+# https://www.humblebundle.com/books/diy-maker-school-make-co-books
+# https://www.humblebundle.com/books/fall-for-crafting-open-road-media-books
+# https://www.humblebundle.com/books/raspberry-pi-press-gaming-books
+# https://www.humblebundle.com/books/learn-to-code-the-fun-way-no-starch-press-books
+# https://www.humblebundle.com/books/start-something-new-chronicle-books
+# https://www.humblebundle.com/books/home-sweet-home-quarto-books
+# https://www.humblebundle.com/books/multilanguage-tales-warhammer-2021-black-library-books
+# https://www.humblebundle.com/books/make-your-own-magic-inventions-make-co-books
+
+
 def order_humble_items(soup):
     # div class="main-content-row dd-game-row js-nav-row"
     # div class="dd-image-box-list js-slideout-items-row"
@@ -29,80 +39,92 @@ def order_humble_items(soup):
     return titles
 
 
-def main():
-    # https://www.humblebundle.com/books/diy-maker-school-make-co-books
-    # https://www.humblebundle.com/books/fall-for-crafting-open-road-media-books
-    # https://www.humblebundle.com/books/raspberry-pi-press-gaming-books
-    # https://www.humblebundle.com/books/learn-to-code-the-fun-way-no-starch-press-books
-    # https://www.humblebundle.com/books/start-something-new-chronicle-books
-    # https://www.humblebundle.com/books/home-sweet-home-quarto-books
-    url = True
-    title_tiers = None
-    if url:
-        current_url = "https://www.humblebundle.com/books/home-sweet-home-quarto-books"
+def generate_author_publisher_string(list_of_names, key1, key2):
+    return ','.join(map(lambda item_in_list: "{key1} ({key2})".format(
+        key1=item_in_list.get(key1, ''),
+        key2=item_in_list.get(key2, '')), list_of_names))
+
+
+def get_bundle_dict(humble_url, is_file):
+    if is_file:
+        # leo el JSON desde un fichero
+        with open(humble_url, "r") as content:
+            bundle_dict = json.load(content)
+    else:
         session = requests.Session()
-        current_page = session.get(current_url)
+        current_page = session.get(humble_url)
         if current_page.status_code != requests.codes.ok:
             exit(-1)
         soup = BeautifulSoup(current_page.text, features="html.parser")
         title_tiers = order_humble_items(soup)
         bundle_vars = soup.find('script', {'id': 'webpack-bundle-data'})
         bundle_dict = json.loads(bundle_vars.string)
+        bundle_dict['title_tiers'] = title_tiers
+    return bundle_dict
+
+
+def build_bundle_dict(humble_items):
+    items_dict = {}
+    for item in humble_items:
+        try:
+            items_dict[item['machine_name']] = {
+                'name': item['human_name'],
+                'author': generate_author_publisher_string(item['developers'], 'developer_name', 'developer_url'),
+                'publisher': generate_author_publisher_string(item['publishers'], 'publisher_name',
+                                                              'publisher_url'),
+                'description': item['description_text']
+            }
+        except Exception as e:
+            # print('Error en display_items: ' + str(e))
+            pass
+    return items_dict
+
+
+def item_in_bundle_dict_to_str(item):
+    return "{name} - {author}. [{pub}]\n{desc}".format(
+        name=item['name'],
+        author=item['author'],
+        pub=item['publisher'],
+        desc=item['description'])
+
+
+def print_bundle_dict(bundle_dict):
+    title_tiers = bundle_dict['title_tiers']
+    if title_tiers:
+        for idx, tier in enumerate(title_tiers):
+            print('\n\n\n\nTIER ' + str(idx) + '\n\n')
+            for name in tier:
+                item = bundle_dict[name]
+                print(item_in_bundle_dict_to_str(item))
+                print('------------------------------------------------')
     else:
-        # leo el JSON desde un fichero
-        current_url = 'json/raspberry.txt'
-        with open(current_url, "r") as content:
-            bundle_dict = json.load(content)
+        for item in bundle_dict.values():
+            print(item_in_bundle_dict_to_str(item))
+            print('------------------------------------------------')
+
+
+def get_humble(humble_url, is_file=False):
+    url = True
+    title_tiers = None
+    bundle_dict = get_bundle_dict(humble_url, is_file)
     try:
         humble_name = bundle_dict['bundleVars']['product_human_name']
         humble_items = bundle_dict['bundleVars']['slideout_data']['display_items']
-        print(humble_name + '\t(' + current_url + ')')
-        items_dict = {}
-        for item in humble_items.values():
-            try:
-                machine_name = item['machine_name']
-                name = item['human_name']
-                devs = item['developers']
-                author = ''
-                for dev in devs:
-                    author = author + "{sep}{name} ({url})".format(sep=(', ' if author else ''),
-                                                                   name=dev['developer_name'],
-                                                                   url=dev['developer_url'])
-                pubs = item['publishers']
-                publisher = ''
-                for pub in pubs:
-                    publisher = publisher + "{sep}{name} ({url})".format(sep=(', ' if publisher else ''),
-                                                                         name=pub['publisher_name'],
-                                                                         url=pub['publisher_url'])
-                description = item['description_text']
-                items_dict[machine_name] = {
-                    'name': name,
-                    'author': author,
-                    'publisher': publisher,
-                    'description': description
-                }
-            except Exception as e:
-                # print('Error en display_items: ' + str(e))
-                pass
-        if title_tiers:
-            for idx, tier in enumerate(title_tiers):
-                print('\n\n\n\nTIER ' + str(idx) + '\n\n')
-                for name in tier:
-                    item = items_dict[name]
-                    print(item['name'])
-                    print(item['author'])
-                    print(item['publisher'])
-                    print(item['description'])
-                    print('------------------------------------------------')
-        else:
-            for item in items_dict.values():
-                print(item['name'])
-                print(item['author'])
-                print(item['publisher'])
-                print(item['description'])
-                print('------------------------------------------------')
+        print(humble_name + '\t(' + humble_url + ')')
+        items_dict = build_bundle_dict(humble_items.values())
+        print_bundle_dict(items_dict)
     except Exception as e:
         print(str(e))
+
+
+def main():
+    list_of_urls = [
+        'https://www.humblebundle.com/books/stuff-that-kids-love-adams-media-books',
+        'https://www.humblebundle.com/books/lets-cook-together-books',
+        'https://www.humblebundle.com/books/learn-you-more-code-no-starch-press-books'
+    ]
+    for url in list_of_urls:
+        get_humble(url)
 
 
 # Lanzamos la función principal
