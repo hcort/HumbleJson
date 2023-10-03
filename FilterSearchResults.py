f88071c731df0b14ae7fbe77489283154ccc1e1b
hcort <elpadawan@gmail.com>
2023-10-03 13:55:44 +0200

    Fixed false negatives in filtering algorithm

--- FilterSearchResults.py
+++ FilterSearchResults.py
@@ -26,15 +26,22 @@ def filter_search_results(humble_dict, libgen_dict):
     filtered_dict = {}
     for libgen_item in libgen_dict:
         if libgen_dict[libgen_item]['extension'] and libgen_dict[libgen_item]['extension'] in ('epub', 'mobi', 'pdf'):
-            libgen_title = libgen_dict[libgen_item]['title'].lower()
+            libgen_title = list(filter(lambda x: len(x) > 2, libgen_dict[libgen_item]['title'].lower().split()))
+            libgen_title = set(map(sanitize_word, libgen_title))
             title_match = True
+            mismatchs = 3
             for word in humble_title_words:
-                title_match &= (libgen_title.find(word) >= 0)
+                if not (word in libgen_title):
+                    mismatchs -= 1
+                    if mismatchs == 0:
+                        title_match = False
+                        break
             # TODO sometimes humble author may be empty
-            libgen_author = libgen_dict[libgen_item]['author'].lower()
+            libgen_author = list(filter(lambda x: len(x) > 2, libgen_dict[libgen_item]['author'].lower().split()))
+            libgen_author = set(map(sanitize_word, libgen_author))
             author_match = False
             for word in humble_author_words:
-                author_match |= (libgen_author.find(word) >= 0)
+                author_match |= (word in libgen_author)
             if title_match and author_match:
                 filtered_dict[libgen_item] = libgen_dict[libgen_item]
     return filtered_dict
